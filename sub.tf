63958e782c8c9e956f61de0d61bfc33358ed2f1a
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-17 23:41:27 +0530

    Update sub.tf

--- sub.tf
+++ sub.tf
@@ -3,7 +3,7 @@ resource "aws_subnet" "tf-pub-sn-1" {
   vpc_id                  = aws_vpc.tf-vpc.id
   cidr_block              = "192.168.100.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-west-2a"
   tags = {
     Name = "tf-pub-sn-1"
   }
@@ -13,7 +13,7 @@ resource "aws_subnet" "tf-pub-sn-2" {
   vpc_id                  = aws_vpc.tf-vpc.id
   cidr_block              = "192.168.200.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-2b"
+  availability_zone       = "us-west-2b"
   tags = {
     Name = "tf-pub-sn-2"
   }
