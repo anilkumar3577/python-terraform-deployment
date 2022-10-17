0e223be55fec8643f7bc63ead9001cfd5284a3ac
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-17 11:40:23 +0530

    Update kp.tf

--- kp.tf
+++ kp.tf
@@ -1,6 +1,6 @@
 #creating key_pair to attach EC2 Instance
 resource "aws_key_pair" "ohioo" {
-  key_name   = "ohioo"
+  key_name   = "test"
   public_key = tls_private_key.rsa.public_key_openssh
 }
 resource "tls_private_key" "rsa" {
@@ -9,5 +9,5 @@ resource "tls_private_key" "rsa" {
 }
 resource "local_file" "ohioo" {
   content  = tls_private_key.rsa.private_key_pem
-  filename = "ohioo"
+  filename = "test"
 }
