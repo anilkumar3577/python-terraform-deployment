88f3d85d50d22d9c97ed24145422c16b5265b104
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-17 11:49:02 +0530

    Delete data2.sh

--- data2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum -y install httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo yum install git -y
-sudo git clone https://github.com/Akiranred/mario.git /var/www/html
