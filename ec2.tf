3e5be2c0af77d4cc5034a579a3e1e7e7953f90f4
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-17 11:48:35 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating 1st EC2 instance in Public Subnet 
 resource "aws_instance" "CC-ec2" {
-  ami                         = "ami-0568773882d492fc8"
+  ami                         = "ami-08e2d37b6a0129927"
   instance_type               = "t2.micro"
-  key_name                    = "tests"
+  key_name                    = "test"
   vpc_security_group_ids      = ["${aws_security_group.tf-sg.id}"]
   subnet_id                   = aws_subnet.tf-pub-sn-1.id
   associate_public_ip_address = true
@@ -12,17 +12,4 @@ resource "aws_instance" "CC-ec2" {
   }
 }
 
-# Creating 2nd EC2 instance in Public Subnet
-resource "aws_instance" "mario-ec2" {
-  ami                         = "ami-0568773882d492fc8"
-  instance_type               = "t2.micro"
-  key_name                    = "tests"
-  vpc_security_group_ids      = ["${aws_security_group.tf-sg.id}"]
-  subnet_id                   = aws_subnet.tf-pub-sn-2.id
-  associate_public_ip_address = true
-  user_data                   = file("data2.sh")
-  tags = {
-    Name = "mario-ec2"
-  }
-}
 
