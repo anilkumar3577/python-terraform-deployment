f2be4aa24f277d889c375e69b74cced9d1d1674e
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-18 22:43:55 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum -y install git
+sudo yum install python3-pip -y
+sudo mkdir python
+cd python
 sudo git clone https://github.com/anilkumar3577/python-1-car-prediction.git
 ls
 cd python-1-car-prediction
