660392a66cabb949f0197b4352d3a2e9f2927e15
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2022-10-26 18:42:59 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum -y install git
-sudo yum install python3-pip -y
-sudo mkdir python
-cd python
-sudo git clone https://github.com/anilkumar3577/python-1-car-prediction.git
-ls
-cd python-1-car-prediction
-pip3 install -r requirements.txt
-python3 app.py
-screen -m -d python3 app.py 
-
-
+sudo yum -y istall httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
+git clone  /var/www/html/
